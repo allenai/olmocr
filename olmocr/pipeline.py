0992ba3c283c7d546f2595ad37c6c0d7622fdd8f
Jake Poznanski <jakep@allenai.org>
2025-11-03 21:11:38 +0000

    Async scheduling is no good

--- olmocr/pipeline.py
+++ olmocr/pipeline.py
@@ -647,7 +647,6 @@ async def vllm_server_task(model_name_or_path, args, semaphore, unknown_args=Non
         '{"video": 0, "image": 1}',  # Disabling video encoder saves RAM that you can put towards the KV cache, thanks @charitarthchugh
         "--mm-processor-cache-gb", # Disable mm cache, since most input are unique
         "0",
-        "--async-scheduling", # Likely to improve performance but prevents speculative decoding from working
     ]
 
     if args.gpu_memory_utilization is not None:
